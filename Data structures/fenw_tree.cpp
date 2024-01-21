a6ff662b0f7537b907b1d4e9dc29e968d82d6688
Nuraziz <ungboyevn@gmail.com>
2024-01-21 14:37:06 +0500

    yanamin

--- Data structures/fenw_tree.cpp
+++ Data structures/fenw_tree.cpp
@@ -18,15 +18,3 @@ int rsq(int i){
 int get(int l , int r){
     return rsq(r) - rsq(l - 1);
 }
- 
-signed main(){
-    int n , m , x , y;
-    cin >> n >> m;
-    t.resize(n + 1);
-    string type;
-    for(int i = 0 ; i < m ; i ++){
-        cin >> type >> x >> y;
-        if(type == "add") add(x , y);
-        else cout << get(x , y) << endl;
-    }
-}
